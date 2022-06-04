d3ce3bb03696c8d02d651807c7785bedc2f05a41
Saoa35 <saoamah@gmail.com>
2022-06-04 20:03:46 +0200

    + statehook example

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,9 +1,10 @@
+import StateHookExample from '../StateHookExample/StateHookExample';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+      <StateHookExample/>
     </div>
   );
 }
