a0ed8fb713fc7ca05f307f25cd8f7be4375951bd
Saoa35 <saoamah@gmail.com>
2022-06-05 19:07:47 +0200

    create methodth & states

--- src/Components/MemoHookExample/MemoHookExample.js
+++ src/Components/MemoHookExample/MemoHookExample.js
@@ -1,14 +1,34 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
+
+const computedFunc = (state) => {
+    return state * 2;
+}
 
 const MemoHookExample = () => {
-    
+
     const [state, setState] = useState(1);
+    const [state2, setState2] = useState(1);
+
+    const computed = useMemo(() => computedFunc(state), [state]) ;
 
     return(
         <div>
             <h2>
                 Memo Hook Example
             </h2>
+
+            <h4>
+                comp: {computed}, state 2: {state2}
+            </h4>
+
+            <p>
+                <button onClick={() => setState(state + 1)}>
+                    Click!
+                </button>
+                <button onClick={() => setState2(state2 + 1)}>
+                    Click me!
+                </button>
+            </p>
         </div>
     )
 }
