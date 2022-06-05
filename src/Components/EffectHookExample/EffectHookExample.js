bc9fbc26e19a55958681d7895336cb3020f292f9
Saoa35 <saoamah@gmail.com>
2022-06-05 12:01:11 +0200

    create hook func

--- src/Components/EffectHookExample/EffectHookExample.js
+++ src/Components/EffectHookExample/EffectHookExample.js
@@ -1,1 +1,14 @@
 import React from 'react';
+
+const EffectHookExample = () => {
+
+    return (
+        <div>
+            <h2>
+                Effect Hook Example
+            </h2>
+        </div>
+    )
+}
+
+export default EffectHookExample;
