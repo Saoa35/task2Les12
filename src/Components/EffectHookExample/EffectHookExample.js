f94d1c5f21bafc778bbbeebd158ed87dbcf74869
Saoa35 <saoamah@gmail.com>
2022-06-05 15:03:41 +0200

    + useEffect method

--- src/Components/EffectHookExample/EffectHookExample.js
+++ src/Components/EffectHookExample/EffectHookExample.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const EffectHookExample = () => {
+
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        console.log('render in useEffect');
+
+        const url = 'https://jsonplaceholder.typicode.com/todos/';
+
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setData(data));
+    }, []);
+
+    console.log('render', data);
 
     return (
         <div>
             <h2>
                 Effect Hook Example
             </h2>
+
+            {data ? data.map(item => <p key={item.id}>{item.title}</p>) : 'Loading...'}
         </div>
     )
 }
